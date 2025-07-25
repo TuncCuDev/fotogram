34ca10aaf4483825a616422e2755f90d493c40d5
TuncCu <tuncay-cubukcu@hotmail.de>
2025-07-25 07:25:59 +0200

    create function for images

--- fotogram.js
+++ fotogram.js
@@ -1,22 +1,30 @@
 let myImgs = [
-    "Mountain_img(1)",
-    "Mountain_img(2)",
-    "Mountain_img(3)",
-    "Mountain_img(4)",
-    "Mountain_img(5)",
-    "Mountain_img(6)",
-    "Mountain_img(7)",
-    "Mountain_img(8)",
-    "Mountain_img(9)",
-    "Mountain_img(10)",
-    "Mountain_img(11)",
-    "Mountain_img(12)",
-    "Mountain_img(13)",
+    "Mountain_img(1).jpg",
+    "Mountain_img(2).jpg",
+    "Mountain_img(3).jpg",
+    "Mountain_img(4).jpg",
+    "Mountain_img(5).jpg",
+    "Mountain_img(6).jpg",
+    "Mountain_img(7).jpg",
+    "Mountain_img(8).jpg",
+    "Mountain_img(9).jpg",
+    "Mountain_img(10).jpg",
+    "Mountain_img(11).jpg",
+    "Mountain_img(12).jpg",
+    "Mountain_img(13).jpg",
 ];
 
 
-function myImgs(i){
+function render(){
     let ImgsRef = document.getElementById('mountain_imgs');
-    ImgsRef.innerHTML = ""
-    for (let i = 0; i < myImgs.length; i++) 
+    ImgsRef.innerHTML = "";
+    for (let i = 0; i < myImgs.length; i++) {
+        mountain_imsRef.innerHTML += getImages(i);
+    }   
+}
+
+function getImages(i){
+    return `<div> 
+                ${myImgs[i]}
+            </div>`
 }
