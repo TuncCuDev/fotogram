0fc03f9568f05c9f3cfbc4be1cc0e37d530f93a6
TuncCu <tuncay-cubukcu@hotmail.de>
2025-07-25 11:43:58 +0200

    change img name mountain

--- fotogram.js
+++ fotogram.js
@@ -1,17 +1,17 @@
 let myImgs = [
-    "Mountain_img(1).jpg",
-    "Mountain_img(2).jpg",
-    "Mountain_img(3).jpg",
-    "Mountain_img(4).jpg",
-    "Mountain_img(5).jpg",
-    "Mountain_img(6).jpg",
-    "Mountain_img(7).jpg",
-    "Mountain_img(8).jpg",
-    "Mountain_img(9).jpg",
-    "Mountain_img(10).jpg",
-    "Mountain_img(11).jpg",
-    "Mountain_img(12).jpg",
-    "Mountain_img(13).jpg",
+    "./img/Mountain1.jpg",
+    "./img/Mountain2.jpg",
+    "./img/Mountain3.jpg",
+    "./img/Mountain4.jpg",
+    "./img/Mountain5.jpg",
+    "./img/Mountain6.jpg",
+    "./img/Mountain7.jpg",
+    "./img/Mountain8.jpg",
+    "./img/Mountain9.jpg",
+    "./img/Mountain10.jpg",
+    "./img/Mountain11.jpg",
+    "./img/Mountain12.jpg",
+    "./img/Mountain13.jpg",
 ];
 
 
@@ -19,12 +19,14 @@ function render(){
     let ImgsRef = document.getElementById('mountain_imgs');
     ImgsRef.innerHTML = "";
     for (let i = 0; i < myImgs.length; i++) {
-        mountain_imsRef.innerHTML += getImages(i);
+        ImgsRef.innerHTML += getImages(i);
     }   
 }
 
 function getImages(i){
     return `<div> 
-                ${myImgs[i]}
+               <img src="${myImgs[i]}" alt="Berbild">
             </div>`
 }
+
+ window.onload = render;
