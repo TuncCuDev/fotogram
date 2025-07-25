3812c69cbba6d443ba5761dabc986ba13157bacb
TuncCu <tuncay-cubukcu@hotmail.de>
2025-07-25 06:55:43 +0200

    change the name of div tag to mountain imgs

---
+++ fotogram.js
@@ -0,0 +1,22 @@
+let myImgs = [
+    "Mountain_img(1)",
+    "Mountain_img(2)",
+    "Mountain_img(3)",
+    "Mountain_img(4)",
+    "Mountain_img(5)",
+    "Mountain_img(6)",
+    "Mountain_img(7)",
+    "Mountain_img(8)",
+    "Mountain_img(9)",
+    "Mountain_img(10)",
+    "Mountain_img(11)",
+    "Mountain_img(12)",
+    "Mountain_img(13)",
+];
+
+
+function myImgs(i){
+    let ImgsRef = document.getElementById('mountain_imgs');
+    ImgsRef.innerHTML = ""
+    for (let i = 0; i < myImgs.length; i++) 
+}
